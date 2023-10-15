4781e15d0f1fcb53f97d649da7472bd4617351f1
Hamza <137694973+hamza122341414@users.noreply.github.com>
2023-10-15 20:20:18 +0500

    Add files via upload

---
+++ Aa.py
@@ -0,0 +1,24 @@
+def logo():
+
+	os.system('clear')
+	logo = (f'''\033[1;37m                                 	
+888    888          888b     d888          
+888    888          8888b   d8888           
+\033[1;92m888    888          88888b.d88888              
+\033[1;92m8888888888 888  888 888Y88888P888.    
+\033[1;37m888    888 `Y8bd8P' 888 Y888P 888           
+\033[1;37m888    888   X88K   888  Y8P  888                    
+\033[1;92m888    888 .d8""8b. 888   "   888                          
+\033[1;92m888    888 888  888 888       888
+\033[1;37m ---------------------------------------------------
+ Owner    : \033[92;1mHamza
+\033[97;1m GitHub   : No Need
+ Status   : \033[1;91mPERSONAL
+\033[97;1m Version  : 1.6
+ \033[1;92m ok/cp ids will be saved in HxM folderðŸ’”
+ \033[1;37m---------------------------------------------------''')
+	print (logo)
+	os.system('espeak -a 300 " Welcome,   To,  ,Hamza,Tool,"')
+print('I love you I love you I love you I love you ')
+print(' love you I love you I love you I love you I love 💕I love you I love you I love you I love you I love you I love you I love you I love you I love 💕I love you I love you I love you I love you I love you I love you I love you I love you I love 💕I love you I love you I love you I love you I love you I love you I love you I love you I love 💕I love you I love you I love you I love you I love you I love you I love you I love you I love 💕I love you I love you I love you I love you I love you I love you I love you I love you I love 💕I love you I love you I love you I love you I love you I love you I love you I love you I love 💕I love you I love you I love you I love you I love you I love you I love you I love you I love 💕I love you I love you I love you I love you I love you I love you I love you I love you I love 💕I love you I love you I love you I love you I love you I love you I love you I love you I love 💕I love you I love you I love you I love you I love you I love you I love you I love you I love 💕I love you I love you I love you I love you I love you I love you I love you I love you I love 💕 ')
+print ( ' From -MrHamza ');time.sleep(2)
